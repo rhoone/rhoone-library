d635849aae47c44187c413dee01d933c139f49c0
Zhao vistart <i@vistart.me>
2019-05-07 19:14:59 +0800

    Create NewBookTarget.php

---
+++ targets/NewBookTarget.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ *  _   __ __ _____ _____ ___  ____  _____
+ * | | / // // ___//_  _//   ||  __||_   _|
+ * | |/ // /(__  )  / / / /| || |     | |
+ * |___//_//____/  /_/ /_/ |_||_|     |_|
+ * @link https://vistart.name/
+ * @copyright Copyright (c) 2016-2019 vistart
+ * @license https://vistart.name/license/
+ */
+namespace rhoone\library\targets;
+
+/**
+ * Class NewBookTarget
+ * @package rhoone\library\targets
+ * @author vistart <i@vistart.me>
+ */
+abstract class NewBookTarget extends Target
+{
+
+}
